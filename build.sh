759f5af68b713f2551ef61feba5f131dfe959860
Michael Simons <msimons@microsoft.com>
2025-06-17 19:39:24 +0000

    Misc build infra changes

--- build.sh
+++ build.sh
@@ -14,8 +14,8 @@ done
 
 scriptroot="$( cd -P "$( dirname "$source" )" && pwd )"
 
-export DOTNET_CLI_TELEMETRY_OPTOUT=1
-export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
-export DOTNET_MULTILEVEL_LOOKUP=0
+# export DOTNET_CLI_TELEMETRY_OPTOUT=1
+# export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
+# export DOTNET_MULTILEVEL_LOOKUP=0
 
-"$scriptroot/eng/common/build.sh" -sb "$@"
+"$scriptroot/eng/common/build.sh" -build -restore "$@"
